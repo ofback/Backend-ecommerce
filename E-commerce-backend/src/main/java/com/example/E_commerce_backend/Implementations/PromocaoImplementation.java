d0b2a885ddea49b6cde23f29e29b46555d85b9d7
phthedev <pedro.h.santos20@ba.estudante.senai.br>
2025-06-03 23:42:59 -0300

    fix: correções na entidade PROMOCAO

--- E-commerce-backend/src/main/java/com/example/E_commerce_backend/Implementations/PromocaoImplementation.java
+++ E-commerce-backend/src/main/java/com/example/E_commerce_backend/Implementations/PromocaoImplementation.java
@@ -2,43 +2,54 @@ package com.example.E_commerce_backend.Implementations;
 
 import com.example.E_commerce_backend.jpa.PromocaoJpa;
 import com.example.E_commerce_backend.Models.PromocaoModel;
+import com.example.E_commerce_backend.Repositories.PromocaoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-public class PromocaoImplementation {
+@Repository
+public class PromocaoImplementation implements PromocaoRepository {
     private final PromocaoJpa promocaoJpa;
 
+    @Autowired
     public PromocaoImplementation(PromocaoJpa promocaoJpa) {
         this.promocaoJpa = promocaoJpa;
     }
 
+    @Override
     public List<PromocaoModel> buscar() {
         return promocaoJpa.findAll();
     }
 
+    @Override
     public PromocaoModel searchByCode(int id) {
         return promocaoJpa.findById(id).orElse(null);
     }
 
+    @Override
     public void addPromocao(PromocaoModel promocaoModel) {
         promocaoJpa.save(promocaoModel);
     }
-    
+
+    @Override
     public void updatePromocao(int id, PromocaoModel promocaoModel) {
         PromocaoModel promocaoInDb = promocaoJpa.findById(id).orElse(null);
         if (promocaoInDb != null) {
             promocaoInDb.setPrecoReal(promocaoModel.getPrecoReal());
             promocaoInDb.setPrecoPromocional(promocaoModel.getPrecoPromocional());
+
             promocaoJpa.save(promocaoInDb);
         }
     }
 
+    @Override
     public void removePromocao(int id) {
         promocaoJpa.deleteById(id);
     }
-    
+
+    @Override
     public boolean estaVazio() {
-        return promocaoJpa.findAll().isEmpty();
+        return promocaoJpa.count() == 0;
     }
-    
 }
